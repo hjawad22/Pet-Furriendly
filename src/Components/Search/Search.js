2b5614b13f2747115d0d1e52b521e10d3ed9621b
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-05-31 16:23:43 -0400

    Add method to filter objects

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -17,9 +17,16 @@ class Search extends Component {
     return (
       <div className="form-container">
         <form className="form">
-          <input type="text" className="city-inp" onChange={() => this.handleChange()} placeholder="City"></input>
+          <input type="text" className="city-inp" placeholder="City" onChange={() => {
+            this.handleChange()
+            this.props.filterPlaces()
+          }
+          }></input>
+          <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png" alt="spy glass icon" />
         </form>
       </div>
     )
   }
 }
+
+export default Search
