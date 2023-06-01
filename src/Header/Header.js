08e3eb076e04b424f22e6a642483284e06917e6b
Tiala Young <tialayoung@gmail.com>
2023-05-31 20:14:48 -0600

    Add basic styling for now

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
-      <h1>Pet Furriendly</h1>
-      <h2>If you're there, they're there.</h2>
-      <NavLink to='/'>Add logo here</NavLink>
+      {/* Link placeholder below for clickable logo */}
+      <Link to='/' className='logo-link'>Add logo here</Link>
+      <div>
+        <h1>Pet Furriendly</h1>
+        <h2 className='slogan'>If you're there, they're there.</h2>
+      </div>
     </header>
   );
 };
