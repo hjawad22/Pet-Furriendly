2a2b4ff3d27df168b9e5c8a8cf183f055dcd70b3
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-01 22:04:41 -0400

    Change Search from a class to a functional component since there is no need to store data in the state for this component, will also fix bug for clearing search with enter key

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Search.css';
 import propTypes from 'prop-types';
 
-class Search extends Component {
-  constructor(){
-    super()
-    this.state = {
-      city: ''
-    }
-  }
 
-  handleChange = (event) => {
-    this.setState({ city: event.target.value })
-  } 
-
-  render() {
-    return (
-      <div className="search-bar">
-        <div className="form-container">
-          <h2 className="search-label">Search by City:</h2>
-          <form className="form">
-            <input type="text" name="city" className="city-inp" placeholder="City" onChange={(event) => {
-              event.preventDefault();
-              this.handleChange(event);
-              this.props.filterPlaces(this.state.city);
-            }
-            }></input>
-            <img className="search-icon" src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png" alt="spy glass icon" />
-          </form>
-        </div>
+const Search = ({ filterPlaces }) => {
+  return (
+    <div className="search-bar">
+      <div className="form-container">
+        <h2 className="search-label">Search by City:</h2>
+        <form className="form">
+          <input type="text" 
+            name="city"
+            className="city-inp"
+            placeholder="City"
+            onChange={(event) => {
+              filterPlaces(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              event.keyCode === 13 && event.preventDefault();
+            }}></input>
+          <img className="search-icon" src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/search-512.png" alt="spy glass icon" />
+        </form>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Search.protoTypes = {
