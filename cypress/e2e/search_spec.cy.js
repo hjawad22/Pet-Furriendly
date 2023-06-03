fc24c96c3ffa0900b036cc3249bde1a477485c72
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-02 22:27:11 -0400

    Moved beforeEach statement inside of describe block in search cypress test

--- cypress/e2e/search_spec.cy.js
+++ cypress/e2e/search_spec.cy.js
@@ -1,12 +1,13 @@
-beforeEach(() => {
-  cy.intercept('http://localhost:8080/api/v1/places', {
-    'content-type': 'application/json',
-    fixture: 'places',
-  })
-  cy.visit('http://localhost:3000/?city=s')
-})
 
 describe('User can use the search bar to filter places by city name', () => {
+  beforeEach(() => {
+    cy.intercept('http://localhost:8080/api/v1/places', {
+      'content-type': 'application/json',
+      fixture: 'places',
+    })
+    cy.visit('http://localhost:3000/?city=s')
+  })
+  
   it('will see a search bar with an input field and search for a city', () => {
     cy.get('input').type("denver").get('.card-container').should('have.length', 6)
   })
